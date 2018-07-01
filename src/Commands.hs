c7350937392cd90da6d07bfbced96679a19ec3f4
Chris Coffey <cmcof273@gmail.com>
2018-06-30 22:43:34 -0400

    Start building out the parser

--- src/Commands.hs
+++ src/Commands.hs
@@ -2,18 +2,67 @@ module Commands where
 
 import Core
 
-import Options.Applicative
+import Control.Applicative ((<|>))
+import Data.Semigroup ((<>))
+import Data.String (IsString)
+import qualified Data.ByteString as BS
 import GHC.Generics (Generic)
+import Options.Applicative
 
-data InputCommands
-    = StartTracking Category
+data InputCommand
+    = StartTracking Category (Maybe BS.ByteString)
     | StopTracking
-    | SwitchTask {oldCat :: Category, newCat :: Category}
+    | SwitchTask Category
     | DefineCategory Category
     | ListCategories
     | ChangeCategoryName Category Category
     deriving (Eq, Show, Generic)
 
 
-commandParser :: Parser InputCommands
-commandParser = undefined
+commandParser :: Parser InputCommand
+commandParser =
+    startCommand <|> stopCommand <|> switchCommand <|>
+    newCategoryCommand <|> listCatsCommand <|> modifyCategoryCmd
+
+startCommand :: Parser InputCommand
+startCommand =
+    StartTracking <$> (
+    Category
+    <$> strOption
+        (long "category" <>
+        short 'c' <>
+        metavar "CATEGORY" <>
+        help "The Category to track against"
+        )
+    )
+    <*>
+    ( emptyStringToNothing
+    <$> strOption
+        (long "desc" <>
+        short 'd' <>
+        metavar "DESCIRPTION" <>
+        help "An optional description for the task" <>
+        value ""
+    )
+    )
+
+emptyStringToNothing :: (Eq s, IsString s) =>
+    s
+    -> Maybe s
+emptyStringToNothing "" = Nothing
+emptyStringToNothing s = Just s
+
+stopCommand :: Parser InputCommand
+stopCommand = undefined
+
+switchCommand :: Parser InputCommand
+switchCommand = undefined
+
+newCategoryCommand :: Parser InputCommand
+newCategoryCommand = undefined
+
+listCatsCommand :: Parser InputCommand
+listCatsCommand = undefined
+
+modifyCategoryCmd :: Parser InputCommand
+modifyCategoryCmd = undefined
