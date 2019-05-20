5f2386455394c6b8a693c5497f8c7c4aeb347ec7
Chris Coffey <cmcof273@gmail.com>
2019-05-20 07:23:04 -0400

    Tiny d3 toy

--- visualization/src/viz.ts
+++ visualization/src/viz.ts
@@ -1,3 +1,10 @@
 import * as d3 from 'd3'
 
 d3.select('body')
+    .selectAll('p')
+    .data([1,2,3,4,5,6])
+    .enter()
+    .append('p')
+    .text(d => {
+        return "Data element number: " + d;
+    });
--- visualization/webpack.config.js
+++ visualization/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/viz.ts'
@@ -19,4 +20,16 @@ module.exports = {
     filename: 'bundle.js'
     ,path: path.resolve(__dirname, 'dist')
   }
+  ,devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
+  }
+  ,plugins: [
+    new HtmlWebpackPlugin({
+       title: 'My Time'
+      ,template: 'index.html'
+    })
+  ]
 };
